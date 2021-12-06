e7dc3067b82e6f600c6a52dd77f80a913503e509
Dimenzio <Dimenzioderechte@gmail.com>
2021-12-06 23:25:49 +0100

    Day 2

---
+++ Day2/Task1/navigator.js
@@ -0,0 +1,51 @@
+const fs = require("fs");
+const readline = require("readline");
+
+let dataArray = [];
+
+let horizontal = 0;
+let depth = 0;
+
+async function processData() {
+    const fileStream = fs.createReadStream('data.txt');
+  
+    const rl = readline.createInterface({
+      input: fileStream,
+      crlfDelay: Infinity
+    });
+  
+    for await (const line of rl) {
+      dataArray.push(line);
+    }
+
+    return Promise.resolve();
+  }
+  
+
+function handleInput(input) {
+    let splitInput = input.split(" ");
+    let num = parseInt(splitInput[1]);
+    switch(splitInput[0]) {
+        case "forward":
+            horizontal += num;
+            break;
+        case "down":
+            depth += num;
+            break;
+        case "up":
+            depth -= num;
+            break;
+    }
+}
+
+async function main() {
+  await processData();
+
+  for(let i = 0; i < dataArray.length; i++) {
+      handleInput(dataArray[i]);
+  }
+
+  console.log(`Calculated final number: ${horizontal*depth}`);
+}
+
+main();
